ee79666f5868d4e2c20a6d101612a7d3c1c4e4d0
Akshay Joshi <joshi.a@gmail.com>
2012-02-26 00:46:01 -0500

    Add the new rails app.

---
+++ game/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Game::Application.config.secret_token = 'a18dbee77da5715501b6d1b5854af379e8a250c1c7bb07cf30e0d4f8bea5abfff45390efa2b241174a5756ad48e5a5dd6bda68ef80b4e5bd8b09cc4f3139a61c'
