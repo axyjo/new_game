b9612a0178403ab6a9a1af9c514d5b9a86a9851c
Akshay Joshi <joshi.a@gmail.com>
2012-02-29 06:34:32 -0500

    Added model stuff for the users model.

--- game/app/models/user.rb
+++ game/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  attr_accessible :username, :email, :password, :password_confirmation
+
+  validates_uniqueness_of :username
+  validates_uniqueness_of :email
 end
